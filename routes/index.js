ac184a7080bf1c3dc473fa7db9df924549bebd35
connor981332 <connorbunch@outlook.com>
2018-11-02 19:41:16 +0000

    working version

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,22 @@ var fs = require('fs');
 var router = express.Router();
 var request = require("request");
 
+var chat = new Array();
+
 router.get('/', function(req, res, next) {
     res.sendFile('chat.html', { root: 'public' });
 });
 
-router.get('/?', function(req, res, next) {
-   //io.sockets.emit('update-msg', { data: 'this is only a test'});
+router.get('/getChat',function(req,res,next) {
+    console.log("In getchat route");
+    var newChat = req.query.q;
+    if (newChat != "") {
+        if (newChat != null) {
+            chat.push(newChat);
+        }
+    }
+    console.log(chat);
+    res.status(200).json(chat);
 });
 
 module.exports = router;
