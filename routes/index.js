bac788146412a471b4fd69bf6fd8bd3aa9b73086
EC2 Default User <ec2-user@ip-172-31-26-139.us-east-2.compute.internal>
2018-11-02 05:31:05 +0000

    Trying to get socket.io working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var io = require('socket.io');
 var router = express.Router();
 var request = require("request");
 
@@ -7,8 +8,8 @@ router.get('/', function(req, res, next) {
     res.sendFile('chat.html', { root: 'public' });
 });
 
-router.get('/chat', function(req, res, next) {
-    
+router.get('/?', function(req, res, next) {
+   io.sockets.emit('update-msg', { data: 'this is only a test'});
 });
 
 module.exports = router;
